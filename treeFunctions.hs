23e1a4fb7c616123b148c61f5eb84a0c9ebf40f0
Panagiotis Kostopanagiotis <panagiotis.kostopanagiotis@gmail.com>
2018-11-27 18:06:10 +0200

    Implemented update and inorder traversal

--- treeFunctions.hs
+++ treeFunctions.hs
@@ -25,3 +25,34 @@ range_query tree range =
                     t2 = aux_query ( getRight tree ) ( mid + 1, r' ) ( mid + 1, j )
     in
         aux_query tree ( 0, n - 1 ) ( l, r )
+
+update tree upd =
+    let
+        n = cntLeaves tree
+        f = getOp tree
+        ( idx, val ) = upd
+        aux_update tree l r =
+            if l == r then
+                Node val 1 f Empty Empty
+            else if idx <= mid then
+                let
+                    t1 = aux_update ( getLeft tree ) l mid
+                    lroot = getRoot t1
+                in
+                    Node ( f lroot ( getRoot ( getRight tree ) ) ) ( cntLeaves tree ) f t1 ( getRight tree )
+            else
+                let
+                    t2 = aux_update ( getRight tree ) ( mid + 1 ) r
+                    rroot = getRoot t2
+                in
+                    Node ( f ( getRoot ( getLeft tree ) ) rroot ) ( cntLeaves tree ) f ( getLeft tree ) t2
+            where
+                mid = div ( l + r ) 2
+    in
+        aux_update tree 0 ( n - 1 )
+
+get_values ( Node x _ _ Empty Empty ) = [ x ]
+get_values tree = ( get_values t1 ) ++ ( get_values t2 )
+    where
+        t1 = getLeft tree
+        t2 = getRight tree
